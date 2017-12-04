1a43436dde956d3391da6725c469dd197b4ab6b1
Jo-Jo <Joakimnordborg92@gmail.com>
2017-12-04 12:07:16 +0100

    Push including Submarine Model, fps functionality, Wwise, take Photos and more!

---
+++ SubmarineExplorer/Assets/Joakim/ButtonTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : GenericButton {
+
+    public Camera subCam; 
+
+    public override void ButtonPressed(GameObject character)
+    {
+        character.GetComponent<Keyboard_FirstPersonController>().usingCam = true; 
+        Camera.main.enabled = false; 
+        subCam.enabled = true;  
+    }
+}
---
+++ SubmarineExplorer/Assets/LoadingBars/scripts/scene2.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class scene2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine (LoadLevel());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	IEnumerator LoadLevel() {
+		yield return new WaitForSeconds(3);
+		SceneManager.LoadSceneAsync(PlayerPrefs.GetString ("LevelName"));
+
+	}
+
+
+
+}

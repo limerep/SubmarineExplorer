401852e1e7b75098acde5f390b1e7a330be3c743
Jo-Jo <Joakimnordborg92@gmail.com>
2017-12-05 16:23:58 +0100

    Try again commit

---
+++ SubmarineExplorer/Assets/Joakim/Script/GenericCreature.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericCreature : MonoBehaviour {
+
+	// Use this for initialization
+	virtual public void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

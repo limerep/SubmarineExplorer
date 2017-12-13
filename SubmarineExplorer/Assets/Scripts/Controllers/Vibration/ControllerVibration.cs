c8818378f8593466fde27ad83d217bc1bc46e043
Daniel Pineiro Nordlund <daniel.gw8@gmail.com>
2017-12-13 10:49:23 +0100

    Merging my branch with master

---
+++ SubmarineExplorer/Assets/Scripts/Controllers/Vibration/ControllerVibration.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerVibration : MonoBehaviour {
+
+
+    public bool hapticFlag = false;
+    private SteamVR_TrackedObject trackedObject;
+    private SteamVR_Controller.Device device;
+
+    void Start ()
+    {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	
+	void Update ()
+    {
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+        if(hapticFlag)
+        {
+            device.TriggerHapticPulse(1500);
+        }
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        hapticFlag = true;
+    }
+
+    private void OnTriggerExit (Collider other)
+    {
+        hapticFlag = false;
+    }
+}
+
+
+//    If you want longer vibrations, try something like this:
+
+////length is how long the vibration should go for
+////strength is vibration strength from 0-1
+//    IEnumerator LongVibration(float length, float strength)
+//    {
+//        for (float i = 0; i < length; i += Time.deltaTime)
+//        {
+//            SteamVR_Controller.Input([index]).TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
+//        yield return null;
+//    }
+//}
+
+//And for pulsed vibrations
+////vibrationCount is how many vibrations
+////vibrationLength is how long each vibration should go for
+////gapLength is how long to wait between vibrations
+////strength is vibration strength from 0-1
+//IEnumerator LongVibration(int vibrationCount, float vibrationLength, float gapLength, float strength)
+//{
+//    strength = Mathf.Clamp01(strength);
+//    for (int i = 0; i < vibrationCount; i++)
+//    {
+//        if (i != 0) yield return new WaitForSeconds(gapLength);
+//        yield return StartCoroutine(LongVibration(vibrationLength, strength));
+//    }
+//}
+
+
---
+++ SubmarineExplorer/Assets/Submarine-Tools/MeshCombiner/Wizard/MeshCombinerWizard.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MeshCombinerWizard : ScriptableWizard {
+    [SerializeField]
+    private string combinedMeshName = "New Combined Mesh";
+    [SerializeField]
+    private bool optimizeMesh = true;
+    
+    [MenuItem("Submarine Tools/Mesh Combiner...")]
+    static void CombineMeshWizard() {
+        DisplayWizard<MeshCombinerWizard>("Mesh Combiner", "Combine Selected Meshes");
+    }
+
+    private void OnWizardCreate() {
+        // Return if no objects are selected
+        if (Selection.gameObjects.Length <= 0) { return; }
+        if (Selection.activeTransform == null) { return; }
+
+        // Create 2 new objects. One object that will have our new mesh
+        // and one that will be a parent to all merged meshes. 
+        GameObject combinedMeshObject = new GameObject(combinedMeshName);
+        GameObject oldMeshesObject = new GameObject(combinedMeshName + " old meshes");
+
+        // Add a meshcombiner script to the new mesh object.
+        MeshCombiner combiner = combinedMeshObject.AddComponent<MeshCombiner>();
+        combiner.meshes = new List<MeshFilter>();
+
+        // Add all the meshfilter to the combiner and then put the object 
+        // as a child to the object that will hold all old mesh objects.
+        for (int i = 0; i < Selection.gameObjects.Length; i++) {
+            combiner.meshes.Add(Selection.gameObjects[i].GetComponent<MeshFilter>());
+            Selection.gameObjects[i].transform.parent = oldMeshesObject.transform;
+        }
+
+        // Add a new list of materials and renderers to the combiner and
+        // exectue the Combine function
+        combiner.materials = new List<Material>();
+        combiner.renderers = oldMeshesObject.GetComponentsInChildren<MeshRenderer>();
+        combiner.CombineMeshes();
+
+        // Optimize the new mesh.
+        if (optimizeMesh) {
+            MeshUtility.Optimize(combiner.ObjectMesh);
+        }
+
+        // Let the user choose a path and name for the new mesh.
+        string path = EditorUtility.SaveFilePanel("Save mesh asset", "Assets/", combinedMeshName, "asset");
+        path = FileUtil.GetProjectRelativePath(path);
+
+        AssetDatabase.CreateAsset(combiner.ObjectMesh, path);
+        AssetDatabase.SaveAssets();
+
+        // Deactivate the object that hold all old meshes.
+        oldMeshesObject.SetActive(false);
+
+        // Selection.objects needs to take in an array of
+        // objects so we just make an temporary array and 
+        // give it the newly created mesh object.
+        GameObject[] selectedObjects = { combinedMeshObject };
+
+        Selection.objects = selectedObjects;
+        
+        // I haven't made up my mind yet if we should keep this script on the object
+        // so that we can use it to update the object itself later or if we should
+        // give it a new editor script instead.
+        //DestroyImmediate(combinedMeshObject.GetComponent<MeshCombiner>());
+    }
+
+}

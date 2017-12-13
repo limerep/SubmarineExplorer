1a3ff4908550969fad3c6dae9c56e949ff855f77
UTB\fritpe <perfritzen@hotmail.com>
2017-12-13 16:31:17 +0100

    Added basic main menu functionality with button animations

---
+++ SubmarineExplorer/Assets/Scripts/MenuController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    [SerializeField]
+        GameObject mainCanvas, optionsCanvas;
+    [SerializeField]
+        Button startBtn, optionsBtn, backBtn, exitBtn;
+
+        Animator mainAnim, optionsAnim;
+
+    void Awake()
+    {
+        if (mainCanvas && optionsCanvas)
+        {
+            mainCanvas.SetActive(true);
+            optionsCanvas.SetActive(true);
+        }
+        else
+            Debug.Log("Populate the menu canvases plz");
+    }
+
+    void Start () {
+
+        mainAnim = mainCanvas.GetComponent<Animator>();
+        optionsAnim = optionsCanvas.GetComponent<Animator>();
+
+
+        optionsBtn.onClick.AddListener(Options);
+        backBtn.onClick.AddListener(Back);
+        startBtn.onClick.AddListener(StartGame);
+        exitBtn.onClick.AddListener(Exit);
+	}
+
+	void Update ()
+    {
+
+	}
+
+    void Options()
+    {
+        mainAnim.SetTrigger("ScaleDown");
+        optionsAnim.SetTrigger("ScaleUp");
+        
+    }
+
+    void Back()
+    {
+        mainAnim.SetTrigger("ScaleUp");
+        optionsAnim.SetTrigger("ScaleDown");
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void Exit()
+    {
+        Application.Quit();
+    }
+}

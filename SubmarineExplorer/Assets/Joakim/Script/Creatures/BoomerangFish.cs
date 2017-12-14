96b217ec3562c1717367d37582ac64edf6c4e73e
Jo-Jo <Joakimnordborg92@gmail.com>
2017-12-14 15:10:25 +0100

    Test Commit

---
+++ SubmarineExplorer/Assets/Joakim/Script/Creatures/BoomerangFish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomerangFish : GenericCreature {
+
+    string type = "BoomerangFish"; 
+
+
+	// Use this for initialization
+	override public void Start () {
+
+        type = "BoomerangFish";
+	}
+	
+
+    public override string ReturnType()
+    {
+        return type; 
+    }
+}
---
+++ SubmarineExplorer/Assets/Joakim/Script/PhotoScript/Photo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SubExplorer {
+
+
+public class Photo {
+
+    private string name;
+    private Texture2D picture;
+    private List<string > creatures; 
+
+    public Photo(string fishName, Texture2D file, List<string> otherFishes)
+        {
+
+            name = fishName;
+            picture = file;
+            creatures = otherFishes; 
+        }
+
+    public string getName()
+    {
+        return name; 
+    }
+    public Texture2D getTexture()
+    {
+         return picture; 
+    }
+        public List<string> getCreatures()
+        {
+            return creatures; 
+        }
+}
+}

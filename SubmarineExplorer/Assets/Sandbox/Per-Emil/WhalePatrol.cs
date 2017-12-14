554655de29295e8119f84c11f536cae251d10e59
UTB\fritpe <perfritzen@hotmail.com>
2017-12-14 16:20:21 +0100

    testing changes in menu

---
+++ SubmarineExplorer/Assets/Sandbox/Per-Emil/WhalePatrol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhalePatrol : MonoBehaviour {
+
+    public int moveSpeed = 1;  //per second 
+    Vector3 computerDirection = Vector3.left;
+    Vector3 moveDirection = Vector3.zero;
+    Vector3 newPosition = Vector3.zero;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Vector3 newPosition = new Vector3(-1, 0, 0) * (moveSpeed * Time.deltaTime);
+        newPosition = transform.position + newPosition;
+        newPosition.x = Mathf.Clamp(newPosition.x, -101, 126);
+        transform.position = newPosition;
+        if (newPosition.x > 126)
+        {
+            newPosition.x = 126;
+            computerDirection.x *= -1;
+        }
+        else if (newPosition.x < -101)
+        {
+            newPosition.x = -101;
+            computerDirection.x *= 1;
+        }
+    }
+}

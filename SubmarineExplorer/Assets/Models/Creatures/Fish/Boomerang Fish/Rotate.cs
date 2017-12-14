82060fcfd72da75734f6442cd6ddfd47705aff62
Tim Rundström <tim_rundstrom_97@hotmail.com>
2017-12-14 13:47:32 +0100

    Added 3 new models with some animations.

---
+++ SubmarineExplorer/Assets/Models/Creatures/Fish/Boomerang Fish/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float speedX = 5f;
+    public float speedY = 5f;
+    public float speedZ = 5f; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(speedX, speedY, speedZ);
+	}
+}

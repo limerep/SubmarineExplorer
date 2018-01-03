52bd6a7e3275581a9f11949db6d86dfea72857ba
Richard Norqvist <richard.norqvist@gmail.com>
2018-01-03 11:21:18 +0100

    Adding navigationsystem

---
+++ SubmarineExplorer/Assets/Sandbox/Richard/Scripts/NavAreaMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavAreaMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 oldPos = transform.position;
+            oldPos.y += Time.deltaTime;
+            transform.position = oldPos;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 oldPos = transform.position;
+            oldPos.y -= Time.deltaTime;
+            transform.position = oldPos;
+        }
+    }
+}
